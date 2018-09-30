e6bfb18b3b540aa7c8e0be1698b6a69171cb4a7b
Himanshu Minocha <manshu4@gmail.com>
2018-09-30 11:26:50 -0500

    Maybe works

--- Parser.py
+++ Parser.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 from scipy import stats
+import matplotlib.pyplot as plt
 
 def groupBuildings():
 
@@ -53,8 +54,6 @@ def groupBuildings():
                 currentWeights = []
                 currentTimes = []
 
-
-    print(allBuildingsList)
     return allBuildingsList
 
 
@@ -92,36 +91,94 @@ def filter():
 
 
             if buildingsList[i]["weights"][j] > buildingsList[i]["weights"][j + 1]:
-                print(buildingsList[i]["weights"][j], buildingsList[i]["weights"][j+1])
+                # print(buildingsList[i]["weights"][j], buildingsList[i]["weights"][j+1])
                 buildingsList[i]["weights"].pop(j+1)
                 buildingsList[i]["times"].pop(j+1)
             j += 1
+
 
     return buildingsList
 
 
 def timeWindow():
     buildingsList = filter()
+    #23 years, 31536000 seconds per year
+    maxTime = 725328000
+    timeIncrement = 31536000
+    #0th index is 1st year, 1st index is 2nd year, up to 23rd index is 22nd year
+    #slopeList will be added to by slopes from each building, then averaged by dividing by number of buildings
+    slopeList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    relevantBuildingsList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    for i in range(0, max(timeArr), 31536000):
-        for j in range(len(weightArr)):
-            #3156000 seconds in a year
-            #default window of 2 years
-            timeWindows = 31536000 * 2
-            maxTime = i + timeWindows
-            if (timeArr[i] <= maxTime and timeArr[i] > i):
-                print()
+    for i in range(len(buildingsList)):
+        if (len(buildingsList[i]["times"])) < 2:
+            continue
+        for j in range(0, maxTime, timeIncrement):
+            for k in range(len(buildingsList[i]["times"]) - 1):
+                subArrayIndices = []
+                while (j <= buildingsList[i]["times"][k] and buildingsList[i]["times"][k] <= j + timeIncrement):
+                    subArrayIndices.append(k)
+                    k += 1
+                    if k == len(buildingsList[i]["times"])-1:
+                        subArrayIndices.append(k)
+                        break
+
+                if len(subArrayIndices) >= 2:
+                    #print(subArrayIndices)
+                    #print(len(buildingsList[i]["times"]))
+                    timeList = []
+                    weightList = []
+
+                    for item in subArrayIndices:
+                        #print(item)
+                        #print(type(item))
+                        #print("item:", buildingsList[i]["times"])
+                        timeList.append(buildingsList[i]["times"][item])
+                        #print("timeList:", timeList)
+                        weightList.append(buildingsList[i]["weights"][item])
+                        #print("weightList:", weightList)
+                        #places slope in right index
+                        #multiply to get yearly decrease
+                    slopeList[int(j / timeIncrement)] += slopeFunction(timeList, weightList)*timeIncrement
+                    relevantBuildingsList[int(j / timeIncrement)] += 1
+
+    for n in range(len(slopeList)):
+        if relevantBuildingsList[n] == 0:
+            continue
+        else:
+            slopeList[n] = slopeList[n] / relevantBuildingsList[n]
 
 
-def testModeling(timeList, weightList):
+    slopeList
+    return slopeList
+
+
+def slopeFunction(timeList, weightList):
     # model = sm.OLS(weightList, timeList).fit()
     # model.summary()
-
     slope, intercept, r_value, p_value, std_err = stats.linregress(timeList, weightList)
-    print(slope)
+
+    # print(slope)
+    return slope
+
+
+def graphSlopes(arrSlopes):
+
+    timeInc = []
+    for i in range(len(arrSlopes)):
+        timeInc.append(1 * i)
+
+    year = [1960, 1970, 1980, 1990, 2000, 2010]
+
+    plt.plot(timeInc, arrSlopes, color='orange')
+    plt.xlabel('Time Intervals')
+    plt.ylabel('Rate of Deterioration')
+    plt.title('Deterioration Over Time')
+    plt.show()
 
 
 
-groupBuildings()
-print(filter())
-print(testModeling([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
+# graphSlopes([44.91, 58.09, 78.07, 107.7, 138.5, 170.6])
+
+# print(filter())
+print(timeWindow())
